3562c312a933439981cc44dad06b1b7fb762c249
Davut Çelik <davut.celik2002d@gmail.com>
2024-05-18 14:20:39 +0300

    first commit

---
+++ anasayfa.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    slidesPerGroup:1,
+    loop:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
